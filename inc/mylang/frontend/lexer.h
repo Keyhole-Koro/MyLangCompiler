ced2eef95f421162baff64c46530291f9d3e9087
Keyhole-Koro <130579219+Keyhole-Koro@users.noreply.github.com>
2026-07-19 17:57:25 +0900

    Add profile-aware DOM lexer entry points

--- inc/mylang/frontend/lexer.h
+++ inc/mylang/frontend/lexer.h
@@ -5,123 +5,45 @@
 typedef int symbol;
 
 typedef enum {
-    ADD,      // +
-    SUB,      // -
-    //MUL,      // *
-    DIV,      // /
-    MOD,      // %
-    ASSIGN,   // =
-    EQ,       // ==
-    NEQ,      // !=
-    LT,       // <
-    GT,       // >
-    LTE,      // <=
-    GTE,      // >=
-    LAND,      // &&
-    LOR,       // ||
-    NOT,      // !
-    AMPERSAND,
-    //BITAND,   // &
-    BITOR,    // |
-    BITXOR,   // ^
-    BITNOT,   // ~
-    LSH,      // <<
-    RSH,      // >>
-    POST_INC, // ++ (postfix)
-    POST_DEC, // -- (postfix)
-    INC,      // ++
-    DEC,      // --
+    ADD, SUB, DIV, MOD, ASSIGN, EQ, NEQ, LT, GT, LTE, GTE,
+    LAND, LOR, NOT, AMPERSAND, BITOR, BITXOR, BITNOT, LSH, RSH,
+    POST_INC, POST_DEC, INC, DEC,
 
-    ASTARISK,  // *
-    ADDRESS,  // &
-    ARROW,   // ->
-    FAT_ARROW, // =>
+    ASTARISK, ADDRESS, ARROW, FAT_ARROW,
 
-    BOOL,     // bool
-    U8,       // u8
-    U16,      // u16
-    I32,      // i32
-    U32,      // u32
-    CHAR,     // char
-    FLOAT,    // float
-    DOUBLE,   // double
-    VOID,     // void
-    LONG,     // long
-    SHORT,    // short
-    CONST,    // const
-    STATIC,   // static
-    MUT,      // mut
-    REF,      // ref
-    EXTERN,   // extern
-    AUTO,     // auto
-    REGISTER, // register
+    BOOL, U8, U16, I32, U32, CHAR, FLOAT, DOUBLE, VOID, LONG, SHORT,
+    CONST, STATIC, MUT, REF, EXTERN, AUTO, REGISTER,
 
-    IF,       // if
-    ELSE,     // else
-    WHILE,    // while
-    DO,       // do
-    FOR,      // for
-    SWITCH,   // switch
-    CASE,     // case
-    DEFAULT,  // default
-    BREAK,    // break
-    CONTINUE, // continue
-    RETURN,   // return
-    YIELD,    // yield
-    UNCHECKED, // unchecked
-    OF,       // of
-    UNDERSCORE, // _
-    TYPEDEF,  // typedef
-    STRUCT,   // struct
-    UNION,    // union
-    ENUM,     // enum
-    SIZEOF,   // sizeof
-    IMPORT,   // import
-    EXPORT,   // export
-    PACKAGE,  // package
-    FROM,     // from
-    REST,     // rest
+    IF, ELSE, WHILE, DO, FOR, SWITCH, CASE, DEFAULT, BREAK, CONTINUE,
+    RETURN, YIELD, UNCHECKED, OF, UNDERSCORE, TYPEDEF, STRUCT, UNION,
+    ENUM, SIZEOF, IMPORT, EXPORT, PACKAGE, FROM, REST,
 
-    L_PARENTHESES, // (
-    R_PARENTHESES, // )
-    SEMICOLON,     // ;
-    COMMA,         // ,
-    L_BRACE,       // {
-    R_BRACE,       // }
-    L_BRACKET,     // [
-    R_BRACKET,     // ]
-    DOT,           // .
-    QUESTION,      // ?
-    COLON,         // :
-    VERTICAL_BAR,  // |
-    CARET,         // ^
-    HASH,          // #
+    L_PARENTHESES, R_PARENTHESES, SEMICOLON, COMMA, L_BRACE, R_BRACE,
+    L_BRACKET, R_BRACKET, DOT, QUESTION, COLON, VERTICAL_BAR, CARET, HASH,
 
-    NUMBER,        // number
-    STRING_LITERAL, // "abc"
-    CHAR_LITERAL,   // 'a'
-    IDENTIFIER,     // defined by user
-    INLINE,         // inline but not implemented
+    NUMBER, STRING_LITERAL, CHAR_LITERAL, IDENTIFIER, INLINE,
+
+    /* Internal token names are retained temporarily while the DOM parser is
+       migrated. They are emitted only when DOM syntax is explicitly enabled. */
     MLX_TAG_OPEN,
     MLX_TAG_CLOSE,
     MLX_TAG_SELF_CLOSE,
     MLX_CLOSE_TAG_OPEN,
     MLX_TEXT,
 
-    EOT            // end of token
+    EOT
 } TokenKind;
 
 typedef struct Token Token;
 
-struct Token{
-  TokenKind kind;
-  char *value;
-  int line;
-  int col;
-  int length;   // source width in bytes (may differ from strlen(value): hex->dec, escapes, quotes)
-  Token *next;
+struct Token {
+    TokenKind kind;
+    char *value;
+    int line;
+    int col;
+    int length;
+    Token *next;
 };
-
 
 typedef enum {
     MODE_DEFAULT,
@@ -139,23 +61,27 @@ typedef struct {
     int mlx_tag_depth;
     TokenKind last_token_kind;
     bool eot_returned;
+    bool dom_syntax_enabled;
 } LexerContext;
 
 LexerContext *lexer_context_create(char *input);
+LexerContext *lexer_context_create_with_dom(char *input, bool dom_syntax_enabled);
 void lexer_context_destroy(LexerContext *ctx);
 Token *lexer_next_token(LexerContext *ctx);
 
 typedef struct {
-  char *str;
-  symbol kind;
+    char *str;
+    symbol kind;
 } StringTokenKindMap;
 
+/* lexer() keeps the historical lexer-test behavior and enables DOM syntax.
+   Compiler entry points must use lexer_with_dom() with the resolved profile. */
 Token *lexer(char *input);
+Token *lexer_with_dom(char *input, bool dom_syntax_enabled);
 
 char *tokenkind2str(TokenKind kind);
 
-// Reads the given file (expanding #include "..." the same way as readSampleInput)
-// and tokenizes the result in one step.
 Token *lexer_from_file(const char *file_path);
+Token *lexer_from_file_with_dom(const char *file_path, bool dom_syntax_enabled);
 
 #endif
